a08e31bf3355c8ad2235af95358c82c0749fd77b
Olena <efymenko.olena@gmail.com>
2024-04-19 17:32:23 +0300

    Integrate the scripts for writing data to Google sheet
    This implementation in the main.py needs to be improved,
    probably the data can't be written to sheets because of permissions

--- scraping_project/main.py
+++ scraping_project/main.py
@@ -1,41 +1,28 @@
-import requests
-import pandas
-from bs4 import BeautifulSoup
-from urllib.parse import urljoin
+"""Basic script in the project."""
+from scraping_and_save import core_team_data, save_to_csv, save_to_json
+from write_to_google_sheet import create_sheets_service, write_to_sheet
 
 
-def core_team_data(url):
-    http_get_response = requests.get(url)
-    soup = BeautifulSoup(http_get_response.text, 'lxml')
-    base_url = url
-    team_data = []
-    team_members = soup.select('.speakers-list_item')
-    for member in team_members:
-        name_element = member.select_one('h3.speakers-list_item-heading')
-        if name_element:
-            role_element = name_element.find_next('div')
-            image_element = member.select_one('img')
-            image_src = urljoin(base_url, image_element['src'])
-            core_team_member = {
-                'name': name_element.text.strip(),
-                'role': role_element.text.strip(),
-                'image': image_src
-            }
-            team_data.append(core_team_member)
-    return team_data
+def main():
+    """Scrape team data, save it to CSV and JSON, upload to Google Sheets."""
+    # Get team data from the website
+    core_team_info = core_team_data('https://interaction24.ixda.org/')
+
+    # Save data to CSV and JSON files
+    save_to_csv(core_team_info, 'team_data.csv')
+    save_to_json(core_team_info, 'team_data.json')
+
+    # Create Google Sheets service instance
+    sheets_service = create_sheets_service()
+
+    # Upload data to Google Sheets
+    spreadsheet_id = '19P2V7zcbwMGw7lI65YCxhwtcSCM3pdh8CKTwwpGxBa0'
+    range_name = 'Sheet1!A1'
+    data = [['Name', 'Role', 'Image URL']] + [[member['name'], member['role'],
+                                               member['image']] for member
+                                              in core_team_info]
+    write_to_sheet(sheets_service, spreadsheet_id, data, range_name)
 
 
-def save_to_csv(scraped_data, filename):
-    df = pandas.DataFrame(scraped_data)
-    df.to_csv(filename, index=False)
-    
-
-def save_to_json(scraped_data, filename):
-    df = pandas.DataFrame(scraped_data)
-    df.to_json(filename, orient='records', indent=2)
-
-
-core_team_info = core_team_data('https://interaction24.ixda.org/')
-
-save_to_csv(core_team_info, 'team_data.csv')
-save_to_json(core_team_info, 'team_data.json')
+if __name__ == "__main__":
+    main()
